17a1514dead2b68ddf69ed48dea6738fa61a21ad
arttioo <108722727+arttioo@users.noreply.github.com>
2023-01-03 15:42:24 +0200

    upd js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,57 @@
 import throttle from 'lodash.throttle';
 
 // const email = document.querySelector('input');
-const message = document.querySelector('textarea');
+// const message = document.querySelector('textarea');
+// const submit = document.querySelector('button');
 // email.addEventListener('input', onEmailInput);
-message.addEventListener('input', onMessageInput);
+// message.addEventListener('input', throttle(onMessageInput, 500));
+// submit.addEventListener('submit', onFormSubmit);
 
 // function onEmailInput(evt) {
 //   const value = evt.currentTarget.value;
 //   console.log(value);
 // }
-function onMessageInput(evt) {
-  const value = evt.currentTarget.value;
-  localStorage.setItem('feedback-form-state', value);
-  console.log(value);
-}
 
-// const form = document.querySelector('.feedback-form');
-// const submit = document.querySelector('button');
-// form.addEventListener('input', throttle(onFormChange, 500));
-// textareaCheck();
-// function onFormChange(evt) {
-//   const email = form.querySelector('input').value;
-//   const message = form.querySelector('textarea').value;
-
-//   const formWritten = {
-//     email: email,
-//     message: message,
-//   };
-//   localStorage.setItem('feedback-form-state', JSON.stringify(formWritten));
-//   console.log(localStorage.setItem);
-// }
-// submit.addEventListener('button', onFormSubmit);
 // function onFormSubmit(evt) {
 //   evt.preventDefault();
-//   evt.currentTarget.reset();
+//   console.log('Отправляем форму');
+//   evt.cuttentTarget.reset();
+// };
+
+// function onMessageInput(evt) {
+//   const value = evt.currentTarget.value;
+//   localStorage.setItem('feedback-form-state', value);
+//   console.log(value);
 // }
-// // проверка на наличие текста в input
-// function textareaCheck() {
-//   const savedMessage = localStorage.getItem('feedback-form-state');
-//   if (savedMessage) {
-//     console.log(savedMessage);
-//     form.value = JSON.stringify(savedMessage);
-//   }
-// }
+
+const form = document.querySelector('.feedback-form');
+const submit = document.querySelector('button');
+form.addEventListener('input', throttle(onFormChange, 500));
+textareaCheck();
+function onFormChange(evt) {
+  const email = form.querySelector('input').value;
+  const message = form.querySelector('textarea').value;
+
+  const formWritten = {
+    email: email,
+    message: message,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formWritten));
+  console.log(localStorage.setItem);
+}
+submit.addEventListener('button', onFormSubmit);
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('Отправляем форму')
+  evt.currentTarget.reset();
+}
+// проверка на наличие текста в input
+function textareaCheck() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    console.log(savedMessage);
+    form.value = JSON.stringify(savedMessage);
+  }
+}
+
+
